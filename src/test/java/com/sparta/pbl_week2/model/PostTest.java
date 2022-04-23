94ab99efbc89a7b8b45b439e58177997cf1e4846
kiwon kwon <funnykyeon@naver.com>
2022-04-23 12:26:47 +0900

    테스트코드 작성중2 4/22

---
+++ src/test/java/com/sparta/pbl_week2/model/PostTest.java
@@ -0,0 +1,14 @@
+package com.sparta.pbl_week2.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostTest {
+    @Test
+    @DisplayName("정상적으로 게시글이 생성되는지 테스트")
+    void createPost() {
+        //given
+    }
+}
--- src/test/java/com/sparta/pbl_week2/model/UserTest.java
+++ src/test/java/com/sparta/pbl_week2/model/UserTest.java
@@ -1,36 +1,36 @@
-//package com.sparta.pbl_week2.model;
-//
-//import com.sparta.pbl_week2.dto.UserDto;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class UserTest {
-//    @Test
-//    @DisplayName("정상케이스")
-//    void check_Test() {
-//        String user_email = "181818aa@naver.com";
-//        String user_password = "1234aaaa";
-//        String user_password_check = "1234aaaa";
-//        String user_name = "18zzz18";
-//
-//        UserDto.Request requestDto = new UserDto.Request(
-//                user_email,
-//                user_password,
-//                user_password_check,
-//                user_name
-//        );
-//        //when
-//        User user = new User(requestDto);
-//        //then
-//        assertEquals(user_email, user.getUserEmail());
-//        assertEquals(user_password, user.getUserPassword());
-////        assertEquals(user_name, user.getUserName());
-////        뭐지 시발 이건 왜 차이가있는거지?
-//    }
-//
-//
-//
-//
-//}
+package com.sparta.pbl_week2.model;
+
+import com.sparta.pbl_week2.dto.UserDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+    @Test
+    @DisplayName("정상케이스")
+    void check_Test() {
+
+        String user_email = "";
+        String user_password = "1234aaaa";
+        String user_password_check = "1234aaaa";
+        String user_name = "18zzz18";
+
+        UserDto.Request requestDto = new UserDto.Request(
+                user_email,
+                user_name,
+                user_password,
+                user_password_check
+        );
+        //when
+        User user = new User(requestDto);
+        //then
+        assertEquals(user_email, user.getUserEmail());
+        assertEquals(user_password, user.getUserPassword());
+        assertEquals(user_name, user.getUserName());
+    }
+
+
+
+
+}
